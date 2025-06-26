3fc65244d0b79a200d83a2211911002b32d5412a
nataly <genahsvili81@gmail.com>
2025-06-26 20:39:45 +0300

    LoginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,26 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver){
+        setDriver(driver);
+        driver.get("https://trello.com/");
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver,10),this);
+
+    }
+    @FindBy(id = "username")
+    WebElement inputEmail;
+    @FindBy(id = "login-submit")
+    WebElement btnContinue;
+    @FindBy(id = "password")
+    WebElement inputPassword;
+    @FindBy(id = "login-submit")
+    WebElement btnLogin;
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class LoginTests {
+import dto.User;
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class LoginTests extends AppManager {
+    @Test
+    public void loginPositiveTest(){
+        User user =User.builder()
+                .email("genahsvili81@gmail.com")
+                .password("Ben5mira8")
+                .build();
+ new HomePage(getDriver()).clickBtnLogin();
+
+    }
 }
