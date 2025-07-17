c8667c2dd72685f44be658dbcf23fe5b89e91e4c
nataly <genahsvili81@gmail.com>
2025-07-17 20:42:30 +0300

    Smoke tests

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -2,34 +2,65 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.lang.reflect.Method;
+import java.util.Locale;
 
 public class AppManager {
 
-   public Logger logger = LoggerFactory.getLogger(AppManager.class);
+    public Logger logger = LoggerFactory.getLogger(AppManager.class);
 
     private WebDriver driver;
+
 
     public WebDriver getDriver() {
         return driver;
     }
-    @BeforeMethod
-    public void setup(Method method){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        logger.info("Start testing with method ---->>> " +method.getName());
+
+    static String browser = System.getProperty("browser", "chrome");
+
+    public AppManager() {
     }
 
-    @AfterMethod(enabled = false)
-    public void tearDown(Method method){
-        logger.info("Stop testing  ---->>> " +method.getName());
-       if(driver!= null)
-           driver.quit();
+    @BeforeMethod(alwaysRun = true)
+    public void setup(Method method) {
+        //  driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                logger.info("use chrome");
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                logger.info("use firefox");
+                break;
+            default:
+                driver = new ChromeDriver();
+                logger.info("use chrome");
+                break;
+
+        }
+        driver.manage().window().maximize();
+        logger.info("Start testing with method ---->>> " + method.getName());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(Method method) {
+        logger.info("Stop testing  ---->>> " + method.getName());
+        if (driver != null)
+            driver.quit();
 
     }
 }
